7dd14446c1c846f472842d33168eb02bdccde564
Mathis Botteau <123393731+Froglaprod@users.noreply.github.com>
2024-04-23 16:07:30 +0200

    Reponse de connexion

--- Frontend/Mathis/passion-lecture/src/components/Formulaire.vue
+++ Frontend/Mathis/passion-lecture/src/components/Formulaire.vue
@@ -1,39 +1,40 @@
 <template>
     <!--Formulaire de login sans recharger la page-->
     <form @submit.prevent="login">
-      <input type="text" v-model="pseudo" placeholder="Pseudo">
-      <input type="password" v-model="password" placeholder="Mot de passe">
-      <button type="submit">Se connecter</button>
+        <input type="text" v-model="pseudo" placeholder="Pseudo">
+        <input type="password" v-model="password" placeholder="Mot de passe">
+        <button type="submit">Se connecter</button>
     </form>
-  </template>
-  
-  <script>
-  //Import de axios pour effectuer des requetes http
-  import axios from 'axios';
-  
-  // Stock les données
-  export default {
+</template>
+
+<script>
+//Import de axios pour effectuer des requetes http
+import axios from 'axios';
+
+export default {
     data() {
-      return {
-        pseudo: '',
-        password: ''
-      };
+        return {
+            pseudo: '',
+            password: ''
+        };
     },
     //Connexion (requete à l'api)
     methods: {
-      async login() {
-        try {
+        async login() {
+
             //Requete à l'api en envoyant nos données
-          const res = await axios.post('http://localhost:3000/api/login/', {
-            pseudo: this.pseudo,
-            password: this.password
-          });
-          // Renvoie d'une erreur de l'api
-        } catch (error) {
-          console.error('Erreur de connexion :', error);
+            await axios.post('http://localhost:3000/api/login/', {
+                pseudo: this.pseudo,
+                password: this.password
+                //Reponse de l'api
+            }).then((response) => {
+                console.log('Connexion réussite',response);
+                //Erreur de l'api
+            }).catch((error) => {
+                console.error('Erreur lors de la connexion :', error);
+            });
         }
-      }
     }
-  }
-  </script>
-  
+}
+
+</script>
