b6f37a3e5f1b4b14253d06881a97853d50a6e0b9
Mathis Botteau <123393731+Froglaprod@users.noreply.github.com>
2024-05-07 14:20:55 +0200

    Update Details.vue
    Informations complete détails pour le livre

--- Frontend/Mathis/passion-lecture/src/components/Details.vue
+++ Frontend/Mathis/passion-lecture/src/components/Details.vue
@@ -9,19 +9,19 @@
                 <p>Titre : {{ book.title }}</p>
             </div>
             <div>
-                <p>Catégorie : {{ book.category_id }}</p>
+                <p>Catégorie : {{ category.name }}</p>
             </div>
             <div>
-                <p>Autheur : {{ book.author_id }}</p>
+                <p>Auteur : {{ author.name }}</p>
             </div>
             <div>
-                <p>Editeur : {{ book.publisher_id }}</p>
+                <p>Editeur : {{ publisher.name }}</p>
             </div>
             <div>
                 <p>Nombre de pages : {{ book.number_of_pages }}</p>
             </div>
             <div>
-                <p>Moyenne d'apréciations : {{ book.average_ratings }}</p>
+                <p>Moyenne d'appréciations : {{ book.average_ratings }}</p>
             </div>
             <div>
                 <p>Extrait : {{ book.extract_pdf }}</p>
@@ -29,10 +29,6 @@
             <div>
                 <p>Résumé : {{ book.summary }}</p>
             </div>
-        </div>
-        <div v-else>
-            <p>Chargement en cours...</p>
-            <button @click="getBookDetails">Cliquez</button>
         </div>
     </div>
 </template>
@@ -43,22 +39,62 @@ import axios from 'axios';
 export default {
     data() {
         return {
-            book: null
+            book: null,
+            author: null,
+            publisher: null,
+            category: null
         };
     },
     mounted() {
+        this.getBookDetails();
     },
     methods: {
+        // Prend les informations générales pour le livre
         async getBookDetails() {
             const bookId = this.$route.params.id;
             try {
                 const response = await axios.get(`http://localhost:3000/api/books/${bookId}`);
                 this.book = response.data.data;
+
+                await this.getAuthorsNames(this.book.author_id);
+                await this.getPublisherNames(this.book.publisher_id);
+                await this.getCategoryNames(this.book.category_id);
+
             } catch (error) {
                 console.error('Erreur lors de la récupération des détails du livre :', error);
             }
         },
+        // Prend les informations de l'auteur pour le livre
+        async getAuthorsNames(authorId) {
+            try {
+                // Requête GET pour récupérer les informations de l'auteur
+                const response = await axios.get(`http://localhost:3000/api/authors/${authorId}`);
+                this.author = response.data.data;
+            } catch (error) {
+                console.error(`Erreur lors de la récupération de l'auteur :`, error);
+            }
+        },
+        //Prend les informations de l'éditeur pour le livre
+        async getPublisherNames(publisherId) {
 
+            try {
+                // Requete GET pour récupérer les informations de l'éditeur
+                const response = await axios.get(`http://localhost:3000/api/publishers/${publisherId}`);
+                this.publisher = response.data.data;
+            } catch (error) {
+                console.error(`Erreur lors de la récupération de l'éditeur du livre ${book.title} :`, error);
+            }
+        },
+        //Prend les informations de la catégorie pour le livre
+        async getCategoryNames(categoryId) {
+            try {
+                // Requete GET pour récupérer les informations de la category
+                const response = await axios.get(`http://localhost:3000/api/categorys/${categoryId}`);
+                this.category = response.data.data;
+            } catch (error) {
+                console.error(`Erreur lors de la récupération de la catégorie du livre ${book.title} :`, error);
+            }
+        }
     }
 };
 </script>
