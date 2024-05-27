5bfda0616881d9dcb0039860e71ee4f0a43a702a
Mathis Botteau <123393731+Froglaprod@users.noreply.github.com>
2024-05-27 10:24:26 +0200

    alert error login

--- Frontend/Adrian/passion-lecture/src/components/Formulaire.vue
+++ Frontend/Adrian/passion-lecture/src/components/Formulaire.vue
@@ -1,6 +1,4 @@
 <template>
-
-    <!--Formulaire de login sans recharger la page-->
     <form @submit.prevent="login" class="loginDiv">
         <div class="titleFormuler">
             <h1 style="color: white;" >Bienvenue !</h1>
@@ -8,18 +6,24 @@
         <div class="bodyFormuler">
             <div class="textF">Pseudo : </div>
             <input type="text" class="itemF itemPlaceholder" v-model="pseudo" placeholder="Pseudo">
-            <div class="textF">Mot de pass : </div>
+            <div class="textF">Mot de passe : </div>
             <input type="password" class="itemF itemPlaceholder" v-model="password" placeholder="Mot de passe">
             <button type="submit" class="itemF buttonSubmit">Se connecter</button>
             
         </div>    
+        <div v-if="errorMessage" class="popup">
+            <div class="popup-content">
+                <span class="close-btn" @click="errorMessage = ''">&times;</span>
+                <p class="error-message">{{ errorMessage }}</p>
+            </div>
+        </div>
     </form>
     <div>    
   </div>
 </template>
 
 <script>
-//Import de axios pour effectuer des requetes http
+// Import de axios pour effectuer des requetes http
 import axios from 'axios';
 import router from '../router/index.js';
 
@@ -31,20 +35,21 @@ export default {
             logged: false,
             books: null,
             admin: false,
+            errorMessage: '' 
         };
     },
     
-    //Connexion (requete à l'api)
+    // Connexion (requete à l'api)
     methods: {
         async login() {
 
-            //Requete à l'api en envoyant nos données
+            // Requete à l'api en envoyant nos données
             await axios.post('http://localhost:3000/api/login/', {
                 pseudo: this.pseudo,
                 password: this.password,
-                //Reponse de l'api
+                // Reponse de l'api
             }).then((response) => {
-                console.log('Connexion réussite',response);
+                console.log('Connexion réussie',response);
                 const adminRes = response.data.data.admin;
                 console.log(adminRes)
                 this.logged = true;
@@ -56,12 +61,12 @@ export default {
                 }
                 else{
                     router.push({ name: 'ProfileAdmin' }); 
-
                 }
 
-                //Erreur de l'api
+                // Erreur de l'api
             }).catch((error) => {
                 console.error('Erreur lors de la connexion :', error);
+                this.errorMessage = 'Pseudo ou mot de passe incorrect.'; // Définition du message d'erreur
             });
         },
 
@@ -80,7 +85,6 @@ export default {
     align-items: center;
     height:70vh;
     width: 100%;
-    
 }
 .bodyFormuler{
     display: flex;
@@ -89,7 +93,6 @@ export default {
     align-items: center;
     padding: 50px;
     color: white;
-    
 }
 .itemF{
     margin : 15px;
@@ -106,7 +109,6 @@ export default {
     align-self: flex-start;
     color: white;
     font-size: large;
-    
 }
 .buttonSubmit{
     width: 200px;
@@ -116,9 +118,8 @@ export default {
     background-color: #504c64;
     color : white;
     border: 2px solid white;
-border-radius: 20px;
-cursor: pointer;
-
+    border-radius: 20px;
+    cursor: pointer;
 }
 .itemPlaceholder{
     color: white;
@@ -126,5 +127,33 @@ cursor: pointer;
 button:hover {
   background-color: black;
 }
-
+.popup {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: rgba(248, 248, 248, 0.8);
+    padding: 20px;
+    border-radius: 10px;
+    z-index: 1000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+.popup-content {
+    text-align: center;
+}
+.close-btn {
+    color: white;
+    font-size: 20px;
+    cursor: pointer;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+}
+.error-message {
+    color: red;
+    margin-top: 10px;
+}
 </style>
