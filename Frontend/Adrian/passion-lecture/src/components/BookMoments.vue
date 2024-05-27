9ab3ff512dae58f6ae4a6eaa8688c17cf4daa87d
Mathis Botteau <123393731+Froglaprod@users.noreply.github.com>
2024-05-27 08:58:21 +0200

    latest books

--- Frontend/Adrian/passion-lecture/src/components/BookMoments.vue
+++ Frontend/Adrian/passion-lecture/src/components/BookMoments.vue
@@ -1,29 +1,27 @@
 <template>
     <div class="booksMoment">
-        <!-- Affichage des livres par catégorie -->
-        <div v-for="(categoryBooks, categoryName) in booksByCategory" :key="categoryName">
-            <h2>Livres du moments :</h2>
-            <ul class="book-list">
-                <li v-for="book in categoryBooks" :key="book.id" class="book-item">
-                    <div>
-                        <img v-if="book.cover_image" :src="book.cover_image" alt="Image de couverture">
-                    </div>
-                    <div>
-                        <p>Titre : {{ book.title }}</p>
-                    </div>
-                    <div>
-                        <p>Auteur : {{ book.author.name }}</p>
-                    </div>
-                    <div>
-                        <p>Utilisateur : {{ book.customer.pseudo }}</p>
-                    </div>
-                    <!-- Voir détails -->
-                    <router-link :to="{ name: 'bookItem', params: { id: book.id } }">Voir les détails</router-link>
-                </li>
-            </ul>
-        </div>
+        <h2>Livres du moments :</h2>
+        <ul class="book-list">
+            <li v-for="book in latestBooks" :key="book.id" class="book-item">
+                <div>
+                    <img v-if="book.cover_image" :src="book.cover_image" alt="Image de couverture">
+                </div>
+                <div>
+                    <p>Titre : {{ book.title }}</p>
+                </div>
+                <div>
+                    <p>Auteur : {{ book.author.name }}</p>
+                </div>
+                <div>
+                    <p>Utilisateur : {{ book.customer.pseudo }}</p>
+                </div>
+                <!-- Voir détails -->
+                <router-link :to="{ name: 'bookItem', params: { id: book.id } }">Voir les détails</router-link>
+            </li>
+        </ul>
     </div>
 </template>
+
 
 <style scoped>
 .booksMoment{
@@ -61,62 +59,32 @@ import axios from 'axios';
 export default {
     data() {
         return {
-            //Objet qui stock les livres pour chaques catégorie
-            booksByCategory: {},
-            //Stock les détails du livre
-            selectedBook: null
+            latestBooks: []
         };
     },
 
     //Permet d'utiliser la methode quand le composant est monté
     mounted() {
-        this.getBooksByCategory();
+        this.getBooksLatest();
     },
     methods: {
 
-        //Prend tout les livres pour chaque catégorie
-        async getBooksByCategory() {
+        //Prend les 6 dernier livres
+        async getBooksLatest() {
             try {
-                //Get pour prendre toutes les catégories
-                const categoriesResponse = await axios.get('http://localhost:3000/api/categorys');
-
-                //Prend chaque livre individuellement pour chaque catégorie
-                for (const category of categoriesResponse.data.data) {
-                    if(category.id == 2){
-                        const books = await this.getBooksForCategory(category.id);
-                    this.booksByCategory[category.name] = books;
-                    }
-                    
-                
-                }
-
-                //Message d'erreur    
-            } catch (error) {
-                console.error('Erreur lors de la récupération des livres par catégorie :', error);
-            }
-        },
-        //Prend tout les info des livres 
-        async getBooksForCategory(categoryId) {
-            try {
-                //Get pour prendre les infos des livres de chaque catégorie
-                const response = await axios.get(`http://localhost:3000/api/categorys/${categoryId}/books`);
-
-                //Retourne les info des livres (titre, auteur, etc)
+                const response = await axios.get('http://localhost:3000/api/books/latest/latest');
                 const books = response.data.data;
 
-                //Récupére les info de l'auteur 
+                // Récupère les auteurs et utilisateurs pour les livres
                 await this.getAuthorsNames(books);
-
-                //Récupére les info de l'utilisateur 
                 await this.getCustomerNames(books);
+                this.latestBooks = books;
 
-                return books;
-
-                //Message d'erreur 
             } catch (error) {
-                console.error('Erreur lors de la récupération des livres pour la catégorie :', categoryId, error);
+                console.error('Erreur lors de la récupération des derniers livres :', error);
             }
         },
+        
         //Prend les informations de l'autheur pour le livre
         async getAuthorsNames(books) {
             for (let book of books) {
